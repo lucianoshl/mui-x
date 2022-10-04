8d82d36629406c3ce28f498bf231fc23efe99d43
Luciano Lopes <lucianoshl@gmail.com>
2022-10-04 13:29:26 -0300

    Update useDataGridProps.ts
    Signed-off-by: Luciano Lopes <lucianoshl@gmail.com>

--- packages/grid/x-data-grid/src/DataGrid/useDataGridProps.ts
+++ packages/grid/x-data-grid/src/DataGrid/useDataGridProps.ts
@@ -23,7 +23,7 @@ const DATA_GRID_FORCED_PROPS: { [key in DataGridForcedPropsKey]?: DataGridProces
   signature: 'DataGrid',
 };
 
-export const MAX_PAGE_SIZE = 100;
+export const MAX_PAGE_SIZE = 10000;
 
 /**
  * The default values of `DataGridPropsWithDefaultValues` to inject in the props of DataGrid.
